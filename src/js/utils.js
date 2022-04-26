8945bbcb959beaa1a516e6b86d7713a9d06a352f
Jade.Kim <jade.kim@travelflan.com>
2022-04-26 16:36:56 +0100

    feat: fetch comment data and render

--- src/js/utils.js
+++ src/js/utils.js
@@ -18,6 +18,53 @@ class Utils {
 	isHomePage() {
 		return document.location.pathname === this.params.homePagePath;
 	}
+
+	init() {
+		this.addOnLoadedEventListener()
+		//TODO: add click event listener
+	}
+
+	addOnLoadedEventListener() {
+		window.addEventListener('DOMContentLoaded', this.updateComments())
+	}
+
+	async getComments() {
+		try {
+			const endpoint = 'https://my-json-server.typicode.com/telegraph/frontend-exercise/comments'
+			
+			const response = await fetch(endpoint)
+			const data = await response.json()
+			return data
+		} catch (err) {
+			console.log(err)
+		}
+	}
+
+	updateComments() {
+		this.getComments()
+		.then(comments => {
+			comments.forEach(comment => {
+				const {
+					body,
+					name,
+					likes
+				} = comment
+				this.renderComment(name, body, likes)
+			});
+		})
+	}
+
+	renderComment(userName, commentBody, likes) {
+		const commentContainer = document.querySelector('.comments__comment-list')
+		const html = `<li class="comment">
+			<div class="comment__content">
+				<p class="comment__user-name">${userName}</p>
+				<p class="comment__content-body">${commentBody}</p>
+			</div>
+			<button name="likes" type="button"><span>${likes}</span> Likes</button>
+		</li>`
+		commentContainer.insertAdjacentHTML('beforeend', html)
+	}
 }
 
 module.exports = Utils;
