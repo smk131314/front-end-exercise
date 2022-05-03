51d11c5f374e342fca19d820c9b1e174f5640557
Jade.Kim <jade.kim@travelflan.com>
2022-05-03 11:43:29 +0100

    feat: add error message

--- src/js/comments/commentsClient.js
+++ src/js/comments/commentsClient.js
@@ -4,10 +4,13 @@ class CommentsClient {
 			const endpoint = 'https://my-json-server.typicode.com/telegraph/frontend-exercise/comments'
 			
 			const response = await fetch(endpoint)
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+      }
 			const data = await response.json()
 			return data
 		} catch (err) {
-			console.log(err)
+			console.error(`Could not get comments: ${err}`)
 		}
   }
 }
